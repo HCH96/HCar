1998acf0d3e3c8e909b9a99866752294ab47d419
fffuro <ignis.heo@gmail.com>
2017-11-13 10:29:08 +0900

    SWW

--- keycar.py
+++ keycar.py
@@ -20,31 +20,31 @@ GPIO.setwarnings(False)
 pwm_setup()
 
 def onKeyPress(event):
-	key = event.char
+    key = event.char
     text.insert('end', 'You pressed %s\n' % (key))
-	if key == 'w':
-		go_forward(20,0.1)
-	if key == 'a':
-		leftSwingTurn(20,0.1)
-	if key == 's':
-		go_backward(20,0.1)
-	if key == 'd':
-		rightSwingTurn(20,0.1)
-	if key == 'q':
-		leftPointTurn(20,0.1)
-	if key == 'e':
-		rightPointTurn(20,0.1)
+    if key == 'w':
+        go_forward(20,0.1)
+    if key == 'a':
+        leftSwingTurn(20,0.1)
+    if key == 's':
+        go_backward(20,0.1)
+    if key == 'd':
+        rightSwingTurn(20,0.1)
+    if key == 'q':
+        leftPointTurn(20,0.1)
+    if key == 'e':
+        rightPointTurn(20,0.1)
 
 
 
 if __name__ == "__main__":
-	try:
-		root = tk.Tk()
-		root.geometry('300x200')
-		text = tk.Text(root, background='black', foreground='white', font=('Comic Sans MS', 12))
-		text.pack()
-		root.bind('<KeyPress>', onKeyPress)
-		root.mainloop()
-	except KeyboardInterrupt:
-		GPIO.cleanup()
-		pwm_low()
+    try:
+        root = tk.Tk()
+        root.geometry('300x200')
+        text = tk.Text(root, background='black', foreground='white', font=('Comic Sans MS', 12))
+        text.pack()
+        root.bind('<KeyPress>', onKeyPress)
+        root.mainloop()
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        pwm_low()
