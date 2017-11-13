b924543ead2e45d71c690a8002b8ab4d7e2b58df
fffuro <ignis.heo@gmail.com>
2017-11-13 10:12:25 +0900

    Ver 3

--- linetrack.py
+++ linetrack.py
@@ -16,42 +16,48 @@ GPIO.setwarnings(False)
 # =======================================================================
 GPIO.setmode(GPIO.BOARD)
 
-leftmostled=16
-leftlessled=18
-centerled=22
-rightlessled=40
-rightmostled=32
+leftmostled = 16
+leftlessled = 18
+centerled = 22
+rightlessled = 40
+rightmostled = 32
 
 GPIO.setup(leftmostled, GPIO.IN)
 GPIO.setup(leftlessled, GPIO.IN)
 GPIO.setup(centerled,   GPIO.IN)
-GPIO.setup(rightmostled, GPIO.IN) #
-GPIO.setup(rightlessled, GPIO.IN) #
+GPIO.setup(rightmostled, GPIO.IN)  #
+GPIO.setup(rightlessled, GPIO.IN)  #
+
 
 def trackingModule():
-	reli = []
-	reli.append(GPIO.input(leftmostled))
-	reli.append(GPIO.input(leftlessled))
-	reli.append(GPIO.input(centerled))
-	reli.append(GPIO.input(rightlessled))
-	reli.append(GPIO.input(rightmostled))
-	return reli
+    reli = list()
+    reli.append(GPIO.input(leftmostled))
+    reli.append(GPIO.input(leftlessled))
+    reli.append(GPIO.input(centerled))
+    reli.append(GPIO.input(rightlessled))
+    reli.append(GPIO.input(rightmostled))
+    return reli
+
 
 def mover(reli, speed):
-	speed = speed
-	if reli == [0,0,1,1,1] or reli == [0,1,1,1,1] or reli == [0,0,0,1,1] or reli == [0,0,0,0,1]:
-		leftSwingTurn(speed,0.5)
-	if reli == [1,0,0,0,1] or reli == [1,1,0,1,1] or reli == [1,1,1,1,1]:
-		go_forward(speed, 0.5)
-	if reli == [1,1,0,0,0] or reli == [1,1,1,1,0] or reli == [1,1,1,0,0] or reli == [1,0,0,0,0]:
-		rightSwingTurn(speed, 0.5)
-	else:
-		go_forward(speed, 0.5)
+    speed = speed
+    if reli[0] == 0:  # [0,0,0,0,1], [0,0,0,1,1], [0,0,1,1,1], [0,1,1,1,1]
+        alpha = reli.count(1) * 2
+        leftSwingTurn(speed-10 + alpha, 0.5)
+    if reli[0] == 1 and reli[-1] == 1:
+        go_forward(speed, 0.5)
+    if reli[0] == 1:
+        alpha = reli.count(0) * 2  # [1,0,0,0,0], [1,1,0,0,0], [1,1,1,0,0], [1,1,1,1,0]
+        rightSwingTurn(speed-10 + alpha, 0.5)
+    else:
+        go_forward(speed, 0.5)
+
 
 def avoider():
-	leftSwingTurn(20,2)
-	go_forward(20,2)
-	rightSwingTurn(20,2)
+    leftSwingTurn(20, 2)
+    go_forward(20, 2)
+    rightSwingTurn(20, 2)
+
 
 GPIO.setwarnings(False)
 pwm_setup()
@@ -63,12 +69,12 @@ SwingPr = 90
 SwingTr = 0.5
 
 if __name__ == "__main__":
-	while True:
-		try:
-			if getDistance() < mindis:
-				avoider()
-			else:
-				mover(trackingModule(), 20)
-		except KeyboardInterrupt:
-			GPIO.cleanup()
-			pwm_low()
+    while True:
+        try:
+            if getDistance() < mindis:
+                avoider()
+            else:
+                mover(trackingModule(), 20)
+        except KeyboardInterrupt:
+            GPIO.cleanup()
+            pwm_low()
