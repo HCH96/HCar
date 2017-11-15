0dfcd804881bc362fb3071197c1203cb53e9851f
fffuro <fffuro@kookmin.ac.kr>
2017-11-15 11:27:04 +0900

    Update linetrackv3.py

--- linetrackv3.py
+++ linetrackv3.py
@@ -43,12 +43,12 @@ def mover(reli, speed, meth):
     speed = speed
     if reli[4] == 0 or reli == [1,0,1,1,1]:
         alpha = (reli.count(1) - 2) * 4
-        rightSwingTurn(speed + alpha, 0.2)
-    elif reli[0] == 1 and reli[4] == 1:
-        go_forward(speed, 0.2)
+        rightPointTurn(speed + alpha, 0.2)
     elif reli[0] == 0 or reli == [1,1,1,0,1]:
         alpha = (reli.count(1) - 2) * 4
-        leftSwingTurn(speed + alpha, 0.2)
+        leftPointTurn(speed + alpha, 0.2)
+    elif reli[0] == 1 and reli[4] == 1:
+        go_forward(speed, 0.2)
     else:
         pass
 
